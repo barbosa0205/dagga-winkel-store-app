404be14090a9c3064f190d2bb6cdcd1537c9d7fa
barbosa0205 <75592332+barbosa0205@users.noreply.github.com>
2021-10-28 03:56:28 -0500

    finished ProductsPreview component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import {
     productContainer,
+    product,
     infoProductContainer,
 } from '../styles/components/product.module.scss'
 
 export const Product = () => {
     return (
         <div className={`${productContainer}`}>
-            <img src="https://picsum.photos/500" alt="product" />
+            <div className={`${product}`}>
+                {/* <img src="https://picsum.photos/500" alt="product" /> */}
+            </div>
             <div className={`${infoProductContainer}`}>
                 <h3>Producto</h3>
                 <p>$123.99</p>
--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {
     mainContainer,
+    titleContainer,
     productsContainer,
 } from '../styles/components/productsPreview.module.scss'
 import { Product } from './Product'
+import { Link } from 'react-router-dom'
 
 export const ProductsPreview = props => {
     return (
         <div className={`${mainContainer}`}>
-            <h3>{props.title}</h3>
+            <div className={`${titleContainer}`}>
+                <h3>{props.title}</h3>
+                <Link to="/oferts">Ver todos</Link>
+            </div>
             <div className={`${productsContainer}`}>
                 <Product />
                 <Product />
